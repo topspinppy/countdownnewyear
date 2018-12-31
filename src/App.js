4c65e6b4887da03bdbd042f8c25ed52d62a6df93
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2018-12-31 14:04:56 +0700

    countdown init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Countdown from 'react-countdown-now';
 import './App.css';
 
 class App extends Component {
+  state = {
+    img: 'https://www.knowledgesharing.me/static/media/ks_logo.c9df07af.svg',
+  }
+  renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <span> Happy New Year !</span>;
+    } else {
+      return (
+        <ul>
+          <li> <span>{hours}</span> Hours</li>
+          <li> <span>{minutes}</span>Minutes</li>
+          <li> <span>{seconds}</span>Seconds</li>
+        </ul>
+      )
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Happy New Year 2019</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Countdown 
+              date={new Date("Jan 1, 2019 00:00:00").getTime()} 
+              renderer={this.renderer}
+            />
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div>
+          <img src="https://www.knowledgesharing.me/static/media/ks_logo.c9df07af.svg" />
+        </div>
       </div>
     );
   }
